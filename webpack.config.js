8a95fbd2231f0bfbf22f9d1b65960bcbdbcf77d2
Jason L Perry <jasper@ambethia.com>
2017-11-01 23:24:07 -0400

    Babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,14 @@ module.exports = {
     filename: 'bundle.js',
     path: __dirname + '/build'
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
   plugins: [new HtmlWebpackPlugin()]
 }
