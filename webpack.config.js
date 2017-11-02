1b0cc4fec654aaaf3e1204b27a17a3f9e4ee4be0
Jason L Perry <jasper@ambethia.com>
2017-11-01 23:16:33 -0400

    Dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   entry: __dirname + '/src',
   output: {
     filename: 'bundle.js',
     path: __dirname + '/build'
-  }
+  },
+  plugins: [new HtmlWebpackPlugin()]
 }
